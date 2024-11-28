6d93008d8fca3445e3f5b6a158407214a6d141e4
133dada <3080867551@qq.com>
2024-11-28 21:58:44 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-/**
- * Detect Electron renderer process, which is node, but we should
- * treat as a browser.
- */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-if (typeof process !== 'undefined' && process.type === 'renderer') {
-  module.exports = require('./browser.js');
-} else {
-  module.exports = require('./node.js');
-}
+// 获取 DOM 节点
+const container = document.getElementById('app');
+// 创建根
+const root = createRoot(container);
+
+// 渲染 React 组件
+root.render(<App />);
